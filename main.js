5acaaf66c5754a53475494666d03813363442f8a
Octavio2010 <131196314+Octavio2010@users.noreply.github.com>
2024-06-04 15:46:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 //https://teachablemachine.withgoogle.com/models/Cnf8gxHxK/
+
+function setup(){
+
+}
+
+function draw(){
+
+}
+
+//modelo Ml5
+console.log("Ml5",ml5.version)
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Cnf8gxHxK/model.json", modelLoaded)
+function modelLoaded(){
+    
+}
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format:"png",
+    png_quality: 90,
+})
+
+camera = document.getElementById("Camera");
+Webcam.attach(camera)
+
+function takesnapshot(){
+    Webcam.snap(function(data_URI){
+        document.getElementById("result").innerHTML='<img id = "capturada" src = "'+data_URI+'"/>'
+    })
+}
