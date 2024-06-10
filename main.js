b1b8be6463b2ecfb897c58e575fc8bd33fd74f3a
Octavio2010 <131196314+Octavio2010@users.noreply.github.com>
2024-06-10 15:34:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,8 +12,24 @@ function draw(){
 console.log("Ml5",ml5.version)
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Cnf8gxHxK/model.json", modelLoaded)
 function modelLoaded(){
-    
+    console.log("modelo carregado")
 }
+
+function check(){
+    img = document.getElementById("capturada")
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error)
+    }else{
+        console.log(results)
+        document.getElementById("nameObject").innerHTML = "Objeto:" + results [0].label
+        document.getElementById("Confidence").innerHTML = "Precisão:" + results [0].confidence.toFixed(2)
+    }
+}
+
 Webcam.set({
     width: 350,
     height: 300,
